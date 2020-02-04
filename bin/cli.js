1dd608d19c7cb062038e0c81b27c06b18e23aaf1
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2020-02-04 19:09:20 +0100

    Add usage instructions if no arguments are provided

--- bin/cli.js
+++ bin/cli.js
@@ -1,21 +1,20 @@
 #!/usr/bin/env node
 
 const crypto = require('crypto');
-
 const algorithm = 'aes-192-cbc';
-
 const password = process.argv[2];
 
+if (!password) {
+  console.error('Usage: lisk-dex-encrypt-passphrase ${password} "${passphrase}"');
+  process.exit(1);
+}
+
 const key = crypto.scryptSync(password, 'salt', 24);
-
 const iv = Buffer.alloc(16, 0);
-
 const cipher = crypto.createCipheriv(algorithm, key, iv);
+const passphrase = process.argv[3];
 
-const plaintext = process.argv[3];
-
-let encrypted = cipher.update(plaintext, 'utf8', 'hex');
+let encrypted = cipher.update(passphrase, 'utf8', 'hex');
 encrypted += cipher.final('hex');
 
 console.log(encrypted);
-
