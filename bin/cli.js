2457a6102b17cd232d0e3fdb58dccb9e9b468023
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2020-02-04 19:16:02 +0100

    Provide additional feedback in case of incorrect usage

--- bin/cli.js
+++ bin/cli.js
@@ -1,18 +1,25 @@
 #!/usr/bin/env node
 
 const crypto = require('crypto');
-const algorithm = 'aes-192-cbc';
-const password = process.argv[2];
+const ALGORITHM = 'aes-192-cbc';
 
-if (!password) {
-  console.error('Usage: lisk-dex-encrypt-passphrase ${password} "${passphrase}"');
+let password = process.argv[2];
+let passphrase = process.argv[3];
+let tooManyArguments = !!process.argv[4];
+
+if (tooManyArguments) {
+  console.error('Too many arguments provided. Usage: lisk-dex-encrypt-passphrase ${password} "${passphrase}"');
   process.exit(1);
 }
 
-const key = crypto.scryptSync(password, 'salt', 24);
-const iv = Buffer.alloc(16, 0);
-const cipher = crypto.createCipheriv(algorithm, key, iv);
-const passphrase = process.argv[3];
+if (!password || !passphrase) {
+  console.error('Too few arguments provided. Usage: lisk-dex-encrypt-passphrase ${password} "${passphrase}"');
+  process.exit(1);
+}
+
+let key = crypto.scryptSync(password, 'salt', 24);
+let iv = Buffer.alloc(16, 0);
+let cipher = crypto.createCipheriv(ALGORITHM, key, iv);
 
 let encrypted = cipher.update(passphrase, 'utf8', 'hex');
 encrypted += cipher.final('hex');
